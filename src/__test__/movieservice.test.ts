ca2ff184e137efa7d86a6c23f9bcc3bffedf7a0e
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-02-02 12:56:14 +0100

    Add array and mock

--- src/__test__/movieservice.test.ts
+++ src/__test__/movieservice.test.ts
@@ -2,9 +2,45 @@
  * @jest-environment jsdom
  */
 
-import { getData } from './../ts/services/movieservice';
+import { IMovie } from './../ts/models/Movie';
 
 beforeEach (() => {
     document.body.innerHTML="";
 });
 
+const movies: IMovie[] = [
+    {
+        Title: "Men in black",
+        imdbID: "tt0119654",
+        Type: "movie",
+        Poster: "...",
+        Year: "1997",
+    },
+    {
+        Title: "Men in black II",
+        imdbID: "tt0120912",
+        Type: "movie",
+        Poster: "...",
+        Year: "2002",
+    },
+    {
+        Title: "Men in black 3",
+        imdbID: "tt1409024",
+        Type: "movie",
+        Poster: "...",
+        Year: "2012",
+    },
+];
+
+jest.mock("axios", () => ({
+    get: async (url: string) => {
+        return new Promise((resolve, reject) => {
+            if (url.endsWith("error")) {
+                reject([]);
+            }
+            else {
+                resolve (movies);
+            }
+        });
+    }
+}));
